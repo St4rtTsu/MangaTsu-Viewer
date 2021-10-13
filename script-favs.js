b1837997ab13c6c0e5a97c858b4af7cd85c9d77a
St4rtTsu <56929235+St4rtTsu@users.noreply.github.com>
2021-10-13 11:14:37 -0300

    Add files via upload

--- script-favs.js
+++ script-favs.js
@@ -10,205 +10,25 @@ window.onload = function() {
 
 
     doujinList = [
-{
- name: "Tsukurareta Shisen",
- img: "https://t.nhentai.net/galleries/1411803/thumb.jpg",
- link: "https://nhentai.net/g/271662/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Inu Live! Sunshine!!<br>(Love Live! Sunshine!!)",
- img: "https://t.nhentai.net/galleries/1768515/thumb.jpg",
- link: "https://nhentai.net/g/334695/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Victim Girls 12 Another one Bites the Dust<br>(TERA The Exiled Realm of Arborea)",
- img: "https://t.nhentai.net/galleries/1835836/thumb.png",
- link: "https://nhentai.net/g/345678/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Papa no Gyaku Toile Training!<br>Daddy’s Reverse Toilet Training!",
- img: "https://t.nhentai.net/galleries/1789999/thumb.jpg",
- link: "https://nhentai.net/g/338352/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "SHADOWS IN BLOOM<br>(Touhou Project)",
- img: "https://t.nhentai.net/galleries/1803549/thumb.jpg",
- link: "https://nhentai.net/g/340440/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Hatsuiku no Ii Meikko o Ryoujoku.",
- img: "https://t.nhentai.net/galleries/1839044/thumb.jpg",
- link: "https://nhentai.net/g/346321/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Kamiyama Koukou Omanko Kenkyuukai Katsudou Kiroku<br>Kamiyama Highschools Vagina Research Society Activity Record<br>(Hyouka)",
- img: "https://t.nhentai.net/galleries/1220248/thumb.jpg",
- link: "https://nhentai.net/g/232858/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Ore x Cos!! ~Ore ga TS Shite Cosplay Enkou ni Hamaru nante!!~<br>Ore x Cos!! I turned into a girl and got hooked on cosplay prostitution!",
- img: "https://t.nhentai.net/galleries/1782926/thumb.jpg",
- link: "https://nhentai.net/g/338477/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Natsuyasumi no Jiyuu Kenkyuu Riko Choukyou 2-kaime<br>~ Yagai de Zecchou Sasete Miyou! ~",
- img: "https://t.nhentai.net/galleries/1799132/thumb.png",
- link: "https://nhentai.net/g/339587/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Chii-chan Kaihatsu Nikki Color Ban<br>Chii-chan Development Diary Full Color Collection",
- img: "https://t.nhentai.net/galleries/1802415/thumb.jpg",
- link: "https://nhentai.net/g/340020/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "GANGBANG & PUNISHER<br>(Girls und Panzer)",
- img: "https://t.nhentai.net/galleries/1112592/thumb.jpg",
- link: "https://nhentai.net/g/207835/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Fukushuu no Elf Liselotte 3<br>~ Oyako Futanari Les Rape de Akuochi Seishin Houkai! ~<br>Vengeful Elf Liselotte 3 Mother and Sister Futanarape for Evil Mental Collapse",
- img: "https://t.nhentai.net/galleries/1836018/thumb.jpg",
- link: "https://nhentai.net/g/345757/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Fukushuu no Elf Lieselotte 2<br>~ Futanari Kaizou de Boshi Sennou Les Rape! ~<br>Vengeful Elf Liselotte 2 The Futanari Transformation and Sibling Lesbian Violation",
- img: "https://t.nhentai.net/galleries/1836016/thumb.jpg",
- link: "https://nhentai.net/g/345756/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Fukushuu no Elf Liselotte<br>~ Futanari Inmon de Mugen Zecchou! ~<br>Vengeful Elf Liselotte The Futanari Lust Mark of Endless Climax",
- img: "https://t.nhentai.net/galleries/1836015/thumb.jpg",
- link: "https://nhentai.net/g/345755/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Dungeon no Ana Sono<br>(Dungeon ni Deai o Motomeru no wa Machigatteiru Darou ka)",
- img: "https://t.nhentai.net/galleries/1837608/thumb.jpg",
- link: "https://nhentai.net/g/346024/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: `Kanojo wa Mou "Onii-chan" to wa Yonde Kurenai...<br>(Sword Art Online)`,
- img: "https://t.nhentai.net/galleries/1838372/thumb.jpg",
- link: "https://nhentai.net/g/346184/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Toaru Kagaku de Shokushu Yuugi<br>A Certain Scientific Tentacle Game<br>(Toaru Kagaku no Railgun)",
- img: "https://t.nhentai.net/galleries/1789644/thumb.jpg",
- link: "https://nhentai.net/g/338211/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Anna to Majo no Shokushu Yuugi<br>Anna and the Witch's Tentacles<br>(Sennen Sensou Aigis)",
- img: "https://t.nhentai.net/galleries/1788623/thumb.jpg",
- link: "https://nhentai.net/g/338030/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "LOVE BONUS TIME IN LULUHAWA<br>(Fate/Grand Order)",
- img: "https://t.nhentai.net/galleries/1646145/thumb.jpg",
- link: "https://nhentai.net/g/314890/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Saimin Onsei de Hajimete Ikasareta Watashi no 60-punkan<br>My 60 Minutes Being Made to Cum for the First Time by a Hypnosis File",
- img: "https://t.nhentai.net/galleries/1814554/thumb.png",
- link: "https://nhentai.net/g/342352/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Lovemare Ge",
- img: "https://t.nhentai.net/galleries/1556605/thumb.jpg",
- link: "https://nhentai.net/g/298608/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Hontou ni Ita!! Jikan Teishi Oji-san",
- img: "https://t.nhentai.net/galleries/1605460/thumb.jpg",
- link: "https://nhentai.net/g/307902/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Yo! Loli Bitch Ch. 1, 4-5, 9",
- img: "https://t.nhentai.net/galleries/1772954/thumb.jpg",
- link: "https://nhentai.net/g/335470/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Daitai Atteru DraQue Series Aliahan de Eroi Koto Shitetara Level 99 ni Natta Ken<br>(Dragon Quest III)",
- img: "https://t.nhentai.net/galleries/1563317/thumb.jpg",
- link: "https://nhentai.net/g/300037/",
- language: `${UK}`,
- note: ""
-},
-
-{
- name: "Daitai Atteru DraQue Monster Shuu<br>Collection of Monsters You Normally Meet in Dragon Quest<br>(Dragon Quest)",
- img: "https://t.nhentai.net/galleries/1403431/thumb.png",
- link: "https://nhentai.net/g/269972/",
- language: `${UK}`,
- note: ""
-}
+        {
+            name: "Number 1",
+            img: "",
+            link: "",
+            language: `${BRA}`,
+            note: "Status: 000/025"
+        }, {
+            name: "Number 2",
+            img: "",
+            link: "",
+            language: `${JAP}`,
+            note: ""
+        }, {
+            name: "Number 3",
+            img: "",
+            link: "",
+            language: `${UK}`,
+            note: ""
+        },
     ];
 
 
@@ -237,7 +57,7 @@ window.onload = function() {
             doujinList[p].note = "";
         }
     
-        getId.innerHTML += '<img src="' + doujinList[p].img + '"><div class="info"><br>' + doujinList[p].language + '<a class="link" href="' + doujinList[p].link + '" target="_blank"><h1>' + doujinList[p].name + '</a></h1></div><div><h1>' + doujinList[p].note + '</h1></div></br>'
+        getId.innerHTML += '<a href="' + doujinList[p].link + '" target="_blank"><img class="cover" src="' + doujinList[p].img + '"><div class="info"></a><br>' + doujinList[p].language + '<a class="link" href="' + doujinList[p].link + '" target="_blank"><h1>' + doujinList[p].name + '</a></h1></div><div><h1>' + doujinList[p].note + '</h1></div></br>'
         document.querySelector("#fav-list").innerHTML = `❤ ${doujinList.length} |` + ' Bookmarks'
 console.log(`ID ▸ ${p + 1} | NAME ▸ ${doujinList[p].name}`)
 console.log(`LANG ▸ ${doujinList[p].language}`)
--- script.js
+++ script.js
@@ -1,129 +1,177 @@
+var EDI = document.querySelector("#EDI")
+var page = 1
+var format = "jpg"
+var selectImg = document.querySelector("#imagens")
 
-    
-    var EDI = document.querySelector("#EDI")
-    var page = 1
-    var format = "jpg"
-    var selectImg = document.querySelector("#imagens")
+function showGear() {
+document.querySelector("#jumper").style.display = "";
+document.querySelector("#ranger").style.display = "";
+document.querySelector("#upperButtons").innerHTML = "<button onclick='nextImg()'>◀</button><button onclick='backImg()'>▶</button>";
+document.querySelector("#lowerButtons").innerHTML = "<button onclick='nextImg()'>◀</button><button onclick='backToTop()'>▲</button><button onclick='backImg()'>▶</button>";
+document.querySelector("#lowerButtons").innerHTML = "<button onclick='nextImg()'>◀</button><button onclick='backToTop()'>▲</button><button onclick='backImg()'>▶</button>";
+document.querySelector("#configSelection").innerHTML = "<button onclick='selectSettingsType()'>⚙</button>";
+document.querySelector("#imagens").style.display = "";
+document.querySelector("#gearHr").style.display = "";
+document.querySelector("#topPageNumber").style.display = "";
+document.querySelector("#bottomPageNumber").style.display = "";
+  
 
-    function hideAll() {
+
+}
+
+function imgBtnsSize() {
+    var WAAA = document.querySelector('#image').offsetHeight;
+    var WEEE = document.querySelector('#image').offsetWidth / 2 - 40;
+
+    document.querySelector('#A').style.height = `${WAAA}` + 'px';
+    document.querySelector('#B').style.height = `${WAAA}` + 'px';
+
+    document.querySelector('#A').style.width = `${WEEE}` + 'px';
+    document.querySelector('#B').style.width = `${WEEE}` + 'px';
+
+
+
+}
+
+function preLoad() {
+var nextPages = page + 3;
+    document.querySelector("#preLoad").innerHTML = "<img class='preLoading' src='" + `${EDI.value}/${nextPages-2}.${format}` + "' style='width:10px;'>";
+    document.querySelector("#preLoad").innerHTML += "<img class='preLoading' src='" + `${EDI.value}/${nextPages-1}.${format}` + "' style='width:10px;'>";
+    document.querySelector("#preLoad").innerHTML += "<img class='preLoading' src='" + `${EDI.value}/${nextPages}.${format}` + "' style='width:10px;'>";
+}
+
+
+function hideAll() {
     document.querySelector("body").style.opacity = "0";
     document.querySelector("body").style.overflowX = "hidden";
-    }
+}
 
-    function showAll() {
+function showAll() {
     document.querySelector("body").style.opacity = "1";
-    }
+}
 
-    function openImg() {
+function openImg() {
     page = 1
-      changeImage(true)
-      pageNumber()
-    }
+    changeImage(true)
 
-    function attImgInfo(file) {
-      document.getElementById('info_img').innerText = `(From: ${file})`
-    }
+    showGear()
+    pageNumber()
+    preLoad()
+    imgBtnsSize()
 
-    function getFileName() {
-      return `${EDI.value}/${page}.${format}`
-    }
+}
 
-    function changeImage(withError) {
-      var image    = document.getElementById('image')
-      var fileName = getFileName()
+function attImgInfo(file) {
+    document.getElementById('info_img').innerHTML = `<span>( From : ${file} )</span>`
+}
 
-      image.src = fileName
+function getFileName() {
+    return `${EDI.value}/${page}.${format}`
+}
 
-      attImgInfo(fileName)
+function changeImage(withError) {
+    var image = document.getElementById('image')
+    var fileName = getFileName()
 
-      if (withError)
+    image.src = fileName
+
+    attImgInfo(fileName)
+
+    if (withError)
         image.onerror = changeFRT
-      else
-        image.onerror = null
-       
-    }
+    else
+        image.onerror = image.src = "./img-error.png"
+
+}
 
 
 
 
-
-    function backImg() {
-      if (page === 1)
+function backImg() {
+    if (page === 1)
         return
 
-      page--
-      changeImage(true)
-      pageNumber()
-    }
+    page--
+    changeImage(true)
+    pageNumber()
+    imgBtnsSize()
+    preLoad()
+}
 
-    function nextImg() {
-      page++
-      changeImage(true)
-      pageNumber()
-    }
+function nextImg() {
+    page++
+    changeImage(true)
+    pageNumber()
+    imgBtnsSize()
+    preLoad()
+}
 
 
-    function changeFRT() {
-      if (format == "jpg") {
+function changeFRT() {
+    if (format == "jpg") {
         format = "png"
-      }
-      else if (format == "png") {
+    } else if (format == "png") {
         format = "jpg"
-      }
-
-      var fileName = getFileName()
-      
-      changeImage(false);
     }
 
+    var fileName = getFileName()
 
-    ranger.onchange = function() {
-      var ranger = document.getElementById('ranger')
-      var width = selectImg.width;
-      var image = document.getElementById('image')
+    changeImage(false);
+}
 
-      image.width = ranger.value
+ranger.onchange = function() {
+    var ranger = document.getElementById('ranger')
+    var width = selectImg.width;
+    var image = document.getElementById('image')
 
-    }
+    image.width = ranger.value;
+    imgBtnsSize()
+
+
+}
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function showImageConfig() {
-  document.querySelector("#imgSettings").innerHTML = "<input type='text' id='opacityValue' placeholder='Opacity%'><button onclick='setOpacity()'>OK</button> <br> <input type='text' id='filterValue' placeholder='filter'><button onclick='changeFilter()'>OK</button> <br> <input type='text' id='blendValue' placeholder='blend'><button onclick='changeBlend()'>OK</button> <br> <button onclick='originalSize()'>Auto</button> <button onclick='reSize()'>Slider</button> <button onclick='horReaderEngine()'>⇠</button> <button onclick='closeImgSettings()'>⨉</button>";
+    document.querySelector("#imgSettings").innerHTML = "<input type='text' id='opacityValue' placeholder='Opacity%'><button onclick='setOpacity()'>OK</button> <br> <input type='text' id='filterValue' placeholder='filter'><button onclick='changeFilter()'>OK</button> <br> <input type='text' id='blendValue' placeholder='blend'><button onclick='changeBlend()'>OK</button> <br> <button onclick='originalSize()'>Auto</button> <button onclick='reSize()'>Slider</button> <button onclick='horReaderEngine()'>⇠</button> <button onclick='closeImgSettings()'>⨉</button>";
 }
 
-function showVertConfig(){ 
-  document.querySelector("#imgSettings").innerHTML = "<div id='vertConfig'> <input type='text' id='pageLimitValue' placeholder='setPageLimit'><button onclick='setPageLimit()'>OK</button> <br> <input type='text' id='vertImgWidth' placeholder='vertImgWidth'> <button onclick='setVertImgWidth()'>OK</button> <br> <input type='text' id='opacityValue' placeholder='Opacity%'><button onclick='setOpacity()'>OK</button> <br> <input type='text' id='filterValue' placeholder='filter'><button onclick='changeFilter()'>OK</button> <br> <input type='text' id='blendValue' placeholder='blend'><button onclick='changeBlend()'>OK</button> <br> <button onclick='vertReaderEngine()'>⇣</button> <button onclick='closeImgSettings()'>⨉</button></div>";
+function showVertConfig() {
+    document.querySelector("#imgSettings").innerHTML = "<div id='vertConfig'> <input type='text' id='pageLimitValue' placeholder='setPageLimit'><button onclick='setPageLimit()'>OK</button> <br> <input type='text' id='vertImgWidth' placeholder='vertImgWidth'><button onclick='setVertImgWidth()'>OK</button> <br> <input type='text' id='opacityValue' placeholder='Opacity%'><button onclick='setOpacity()'>OK</button> <br> <input type='text' id='filterValue' placeholder='filter'><button onclick='changeFilter()'>OK</button> <br> <input type='text' id='blendValue' placeholder='blend'><button onclick='changeBlend()'>OK</button> <br> <button onclick='vertReaderEngine()'>⇣</button> <button onclick='multiHorizontal()'>⇠</button> <button onclick='closeImgSettings()'>⨉</button></div>";
+}
+
+function selectSettingsType() {
+    document.querySelector("#imgSettings").innerHTML = "<button onclick='showImageConfig()'>One</button><button onclick='showVertConfig()'>Multi</button>"
 }
 
 function closeImgSettings() {
-  document.querySelector("#imgSettings").innerHTML = "";
+    document.querySelector("#imgSettings").innerHTML = "";
 }
 
 function originalSize() {
-  document.querySelector("#image").style.width = "auto";
+    document.querySelector("#image").style.width = "auto";
 }
 
 function reSize() {
-  document.querySelector("#image").style = "";
+    document.querySelector("#image").style = "";
 }
 
 function changeFilter() {
-  var filterValue = document.querySelector("#filterValue").value
-  document.querySelector("#imagens").style.filter = `${filterValue}`;
+    var filterValue = document.querySelector("#filterValue").value
+    document.querySelector("#imagens").style.filter = `${filterValue}`;
 }
 
 function changeBlend() {
-  var blendValue = document.querySelector("#blendValue").value
-  document.querySelector("#imagens").style.mixBlendMode = `${blendValue}`;
+    var blendValue = document.querySelector("#blendValue").value
+    document.querySelector("#imagens").style.mixBlendMode = `${blendValue}`;
 }
 
 function setOpacity() {
-  var opacityValue = document.querySelector("#opacityValue").value
-  document.querySelector("#imagens").style.opacity = `${opacityValue}`;
+    var opacityValue = document.querySelector("#opacityValue").value
+    document.querySelector("#imagens").style.opacity = `${opacityValue}`;
 
 }
 
@@ -135,11 +183,17 @@ function setPageLimit() {
     document.querySelector(".bbuttons").style.display = "none";
     document.querySelector("#ranger").style.display = "none";
     document.querySelector("#jumper").style.display = "none";
-limit = document.querySelector("#pageLimitValue").value
+    document.querySelector("#info_img").style.display = "none";
+    limit = document.querySelector("#pageLimitValue").value
     page = 1
 }
 
 function horReaderEngine() {
+    document.querySelector("#imagens").style.display = "";
+    document.querySelector("#imagens").style.flexDirection = "";
+    document.querySelector("#imgBox").style.marginBottom = "";
+    document.querySelector("#imgBox").style.overflowX = "";
+
     document.querySelector("#imagens").innerHTML = "";
     document.querySelector("#topPageNumber").style.display = "";
     document.querySelector("#bottomPageNumber").style.display = "";
@@ -147,7 +201,9 @@ function horReaderEngine() {
     document.querySelector(".bbuttons").style.display = "";
     document.querySelector("#ranger").style.display = "";
     document.querySelector("#jumper").style.display = "";
-    document.querySelector("#imagens").innerHTML = "<img src='./img-dark.png' id='image' width='250px'>";
+    document.querySelector("#info_img").style.display = "";
+
+    document.querySelector("#imagens").innerHTML = "<div id='imgBtns'><button id='A' class='imgBtns' onclick='nextImg()'>A</button><button id='B' class='imgBtns' onclick='backImg()'>B</button></div><img src='./img-error.png' id='image' width='250px' onmouseover='imgBtnsSize()'>";
     page = 1
     limit = 0
 
@@ -155,89 +211,114 @@ function horReaderEngine() {
 
 function vertReaderEngine() {
 
-if(page > limit) { return; }else {
-   document.querySelector("#imagens").innerHTML += "<img class='vertImg' src='" + `${EDI.value}/${page}.${format}` + "' style='width:200px;'>";
-   page++
-   vertReaderEngine() 
+    if (page > limit) {
+    document.querySelector("#imagens").style.display = "";
+    document.querySelector("#imagens").style.flexDirection = "";
+    document.querySelector("#imgBox").style.marginBottom = "";
+    document.querySelector("#imgBox").style.overflowX = "";
+
+
+    } else {
+        document.querySelector("#imagens").innerHTML += "<img class='vertImg' src='" + `${EDI.value}/${page}.${format}` + "' style='width:200px;'>";
+        page++
+        vertReaderEngine()
+    }
 }
+
+function multiHorizontal() {
+vertReaderEngine()
+
+document.querySelector("#imagens").style.display = "flex";
+document.querySelector("#imagens").style.flexDirection = "row-reverse";
+document.querySelector("#imgBox").style.marginBottom = "20px";
+document.querySelector("#imgBox").style.overflowX = "scroll";
+document.querySelector("#imgBox").style.width = "auto";
+document.querySelector("#imgBox").scrollTo(document.querySelector("#imgBox").scrollWidth, 0)
+
 }
 
 function setVertImgWidth() {
-  var vertImgWidth = document.querySelector("#vertImgWidth").value
-        let vert = document.querySelectorAll(".vertImg");
-        for (let c = 0; c < vert.length; c++) {
-            vert[c].style.width = `${vertImgWidth}`;
+    var vertImgWidth = document.querySelector("#vertImgWidth").value
+    let vert = document.querySelectorAll(".vertImg");
+    for (let c = 0; c < vert.length; c++) {
+        vert[c].style.width = `${vertImgWidth}`;
 
-        }
+    }
 }
 
 function pageNumber() {
-document.querySelector("#topPageNumber").innerText = `${page}`;
-document.querySelector("#bottomPageNumber").innerText = `${page}`;
+    document.querySelector("#topPageNumber").innerText = `${page}`;
+    document.querySelector("#bottomPageNumber").innerText = `${page}`;
 }
 
 
+function jumpToThePage() {
+var xpage = document.querySelector("#jumper").value
+
+if (xpage.length == 0){
+ return;
+}
+else {
+    page = xpage
+    changeImage(true)
+    pageNumber()
+}
+
+}
+
+function showBookmarks() {
+document.querySelector(".things").style.display = "";
+document.querySelector("#closeBookmarks").style.display = "";
+}
+
+function closeBookmarks() {
+document.querySelector(".things").style.display = "none";
+document.querySelector("#closeBookmarks").style.display = "none";
+
+}
 document.onkeydown = checkKey;
 
 function checkKey(e) {
 
     if (e.keyCode == '39') {
-      if (page === 1)
-        return
-      page--
-      changeImage(true)
-      pageNumber()
-      console.log("ESQUERDA")
-    }
-    else if (e.keyCode == '37') {
-      page++
-      changeImage(true)
-      pageNumber()
-      console.log("DIREITA")
-    }
-    else if (e.keyCode == '68') {
-      if (page === 1)
-        return
-      page--
-      changeImage(true)
-      pageNumber()
-      console.log("ESQUERDA")
-    }
-    else if (e.keyCode == '65') {
-      page++
-      changeImage(true)
-      pageNumber()
-      console.log("DIREITA")
-    }
-    else if (e.keyCode == '13') {
-     openImg();
-     pageNumber()
-    }
-    else if (e.keyCode == '16') {
-    backToTop();
-    }
-    else if (e.keyCode == '83') {
-    window.scrollBy(0, +70);
-    }
-    else if (e.keyCode == '87') {
-    window.scrollBy(0, -70);
-    }
-    else if (e.keyCode == '69') {
-    hideAll()
-    }
-    else if (e.keyCode == '81') {
-    showAll()
-    }
-    else if (e.keyCode == '74') {
-    page = document.querySelector("#jumper").value
-    changeImage(true)
-    pageNumber()
-    }
-    else if (e.keyCode == '72') {
-    showImageConfig()
-    }
-    else if (e.keyCode == '86') {
-    showVertConfig()
+        if (page === 1)
+            return
+        page--
+        changeImage(true)
+        pageNumber()
+        console.log("ESQUERDA")
+    } else if (e.keyCode == '37') {
+        page++
+        changeImage(true)
+        pageNumber()
+        console.log("DIREITA")
+    } else if (e.keyCode == '68') {
+        if (page === 1)
+            return
+        page--
+        changeImage(true)
+        pageNumber()
+        console.log("ESQUERDA")
+    } else if (e.keyCode == '65') {
+        page++
+        changeImage(true)
+        pageNumber()
+        console.log("DIREITA")
+    } else if (e.keyCode == '13') {
+        openImg();
+        pageNumber()
+    } else if (e.keyCode == '16') {
+        backToTop();
+    } else if (e.keyCode == '83') {
+        window.scrollBy(0, +70);
+    } else if (e.keyCode == '87') {
+        window.scrollBy(0, -70);
+    } else if (e.keyCode == '69') {
+        hideAll()
+    } else if (e.keyCode == '81') {
+        showAll()
+    } else if (e.keyCode == '74') {
+jumpToThePage()
     }
 
 
